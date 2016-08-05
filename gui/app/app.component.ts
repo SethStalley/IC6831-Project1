e4bd19e895efe91172b7f233f6aeb3c5d0a752e9
Seth Stalley <sethstalley@gmail.com>
2016-08-05 13:44:35 -0600

    Init

---
+++ gui/app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgStyle} from '@angular/common';
+import {FILE_UPLOAD_DIRECTIVES, FileUploader} from 'ng2-file-upload';
+
+// const URL = '/api/';
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+
+@Component({
+  selector: 'file-upload',
+  templateUrl: 'app/file-uploader.html',
+  directives: [FILE_UPLOAD_DIRECTIVES, NgClass, NgStyle, CORE_DIRECTIVES, FORM_DIRECTIVES]
+})
+
+export class FileUploder {
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
+}
+
---
+++ gui/app/main.ts
@@ -0,0 +1,5 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { FileUploder } from './app.component';
+
+bootstrap(FileUploder);
