33e7a8be100f0e38acc406c71233b429434e4592
Seth Stalley <sethstalley@gmail.com>
2016-08-14 23:21:37 -0600

    Added a Kmeans example

--- backend/complete/src/main/java/webserver/FileUploadController.java
+++ backend/complete/src/main/java/webserver/FileUploadController.java
@@ -5,15 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class FileUploadController {
