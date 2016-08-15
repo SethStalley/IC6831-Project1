97f21f344694483f7281923f93ec7f7ce7dddabe
Seth <sethstalley@gmail.com>
2016-08-14 22:29:45 -0600

    Angular (#1)
    * Init
    
    * Text update
    
    * Moved to a Java Spring webserver.
    
    Init on using Spring backend with Angular2 client
    
    * Can now post multipart files
    
    * Init on notification service
    
    * Added video playback example

---
+++ backend/complete/src/main/resources/public/app/file-uploader.component.ts
@@ -0,0 +1,47 @@
+import {Component, Input} from '@angular/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgStyle} from '@angular/common';
+import {FILE_UPLOAD_DIRECTIVES, FileUploader} from 'ng2-file-upload';
+import {ToastyService, Toasty, ToastOptions, ToastData} from 'ng2-toasty';
+
+// static URL to send out file to;
+const URL = 'http://localhost:8080/postFile';
+
+@Component({
+  selector: 'file-upload',
+  templateUrl: 'app/file-uploader.html',
+  directives: [FILE_UPLOAD_DIRECTIVES, NgClass, NgStyle, CORE_DIRECTIVES, FORM_DIRECTIVES,
+              Toasty]
+})
+
+export class FileUploder {
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  constructor(private toastyService:ToastyService) {
+     this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
+          console.log("VideoUpload:uploaded:", item,response, status);   
+          //Notify Users
+          this.addToast(response);
+     };
+  }
+
+  //Notify Users
+  addToast(msg:string) {
+    if(msg==""){
+      msg = "No hay conneción al servidor!"
+    }
+    this.toastyService.info({
+        title: "",
+        msg: msg,
+        showClose: true,
+        timeout: 5000,
+        theme: "material"
+    });
+  };
+
+}
+
