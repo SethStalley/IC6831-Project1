6955fbe14e456c3e1af792bdbd5a0d014682ac82
Lucy Chaves <lugam_13@hotmail.com>
2016-10-22 13:54:51 -0600

    Checkstyle

--- Implementation/Source Code/backend/complete/src/main/java/teamidentifier/PlayerDetector.java
+++ Implementation/Source Code/backend/complete/src/main/java/teamidentifier/PlayerDetector.java
@@ -1,3 +1,7 @@
+/*
+ * @author Lucy Chaves - Seth Stalley
+ * @version v1.1.1
+ */
 package teamidentifier;
 
 import java.util.ArrayList;
@@ -10,16 +14,25 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 /**
+ * The purpose of this class is to detect the players of the soccer field.
+ * The whole algorithm was taken from the Project Specification written by Saul Calderón.
+ * Players must have their uniform different to green.
+ * The soccer field must be green.
  */
 public class PlayerDetector extends GeneralDetector {
 	private ArrayList<Mat> processedPlayers;
-
+	
+	/**
+  	* Instantiates a new player detector.
+    	*
+  	* 
+  	*/
 	public PlayerDetector() {
 		this.processedPlayers = new ArrayList<Mat>();
 	}
 
 	/**
-	 * Method Detect.
+	 * Detects players in the soccer field.
 	 * 
 	 * @param frames ArrayList<Mat>
 	 */
@@ -28,31 +41,35 @@ public class PlayerDetector extends GeneralDetector {
 		for (Mat frame : frames) {
 			Mat temp = getHueChannel(convertRgb2Hsv(frame));
 			temp = normalizeImage(temp);
-			temp = stdfilt(temp);
-			
+			temp = stdfilt(temp);		
 			temp = dilate(temp);
 			temp = truncate(temp);
 			
 			double umbral = graythresh(temp);
-		    Imgproc.threshold(temp, temp, umbral, 255, Imgproc.THRESH_BINARY);			
+		   	Imgproc.threshold(temp, temp, umbral, 255, Imgproc.THRESH_BINARY);					
+		   	temp = imfill(temp, new Point(0, temp.height()  * 0.70));
 			
-		    temp = imfill(temp, new Point(0, temp.height()  * 0.70));
-
 			this.processedPlayers.add(temp);
 		}
-
 	}
 
-	/**
-	 * @param image Mat
-	 * @return -
-	 */
+  	/**
+  	 * Normalize an image between 2 values
+  	 * Uses OpenCV function: http://docs.opencv.org/java/2.4.2/org/opencv/core/Core.html
+  	 * @param The Image to be normalized, must be in one channel (Mat).
+  	 * @return The normalized OpenCV image (Mat).
+  	 */
 	private Mat normalizeImage(Mat image) {
 		Core.normalize(image, image, 0, 255, Core.NORM_MINMAX);
 		return image;
 	}
 	
-	
+	/**
+  	* Gets optimum threshold of a binary image.
+   	* Uses opencv function: http://docs.opencv.org/java/2.4.9/org/opencv/imgproc/Imgproc.html
+   	* @param The image to get its optimum threshold. In binary format (Mat).
+   	* @return A double with the optimum threshold.
+   	*/	
 	private double graythresh(Mat mat) {
 	   Mat clone = mat.clone();
 	   clone.convertTo(clone, CvType.CV_8UC1);
@@ -64,8 +81,8 @@ public class PlayerDetector extends GeneralDetector {
 	/**
 	 * Calculates local variance.
 	 * 
-	 * @param image Mat
-	 * @return - Image corresponding to the local variance
+	 * @param The image to get its local variance (Mat).
+	 * @return - Image corresponding to the local variance (Mat).
 	 */
 	private Mat stdfilt(Mat image) {
 		Mat image32f = new Mat();
@@ -88,10 +105,11 @@ public class PlayerDetector extends GeneralDetector {
 		return image2;
 	}
 
-	/**
-	 * 
-	 * @param image  Mat
-	 * @return -
+	/** 
+	 * Truncates an image.
+	 * Uses opencv function: http://docs.opencv.org/java/2.4.9/org/opencv/imgproc/Imgproc.html
+	 * @param The image to be truncated (Mat).
+	 * @return - The truncated image (Mat).
 	 */
 	public Mat truncate(Mat image) {
 		Imgproc.threshold(image, image, 0, 255, Imgproc.THRESH_OTSU);
